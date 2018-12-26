774c238c9dbc2294ccb16f1b07fdb0694d72e139
David Kiger <david.kiger@gmail.com>
2018-12-26 00:50:37 -0800

    Finished day 19.

---
+++ 19-2.py
@@ -0,0 +1,101 @@
+file = open("19-1.in", "r")
+
+
+def addr(regs, a, b, c):
+    regs[c] = regs[a] + regs[b]
+    return regs
+
+
+def addi(regs, a, b, c):
+    regs[c] = regs[a] + b
+    return regs
+
+
+def mulr(regs, a, b, c):
+    regs[c] = regs[a] * regs[b]
+    return regs
+
+
+def muli(regs, a, b, c):
+    regs[c] = regs[a] * b
+    return regs
+
+
+def banr(regs, a, b, c):
+    regs[c] = regs[a] & regs[b]
+    return regs
+
+
+def bani(regs, a, b, c):
+    regs[c] = regs[a] & b
+    return regs
+
+
+def borr(regs, a, b, c):
+    regs[c] = regs[a] | regs[b]
+    return regs
+
+
+def bori(regs, a, b, c):
+    regs[c] = regs[a] | b
+    return regs
+
+
+def setr(regs, a, b, c):
+    regs[c] = regs[a]
+    return regs
+
+
+def seti(regs, a, b, c):
+    regs[c] = a
+    return regs
+
+
+def gtir(regs, a, b, c):
+    regs[c] = int(a > regs[b])
+    return regs
+
+
+def gtri(regs, a, b, c):
+    regs[c] = int(regs[a] > b)
+    return regs
+
+
+def gtrr(regs, a, b, c):
+    regs[c] = int(regs[a] > regs[b])
+    return regs
+
+
+def eqir(regs, a, b, c):
+    regs[c] = int(a == regs[b])
+    return regs
+
+
+def eqri(regs, a, b, c):
+    regs[c] = int(regs[a] == b)
+    return regs
+
+
+def eqrr(regs, a, b, c):
+    regs[c] = int(regs[a] == regs[b])
+    return regs
+
+
+lines = file.readlines()
+_, ip_reg = lines[0].split()
+ip_reg = int(ip_reg)
+regs = [1, 0, 0, 0, 0, 0]
+
+lines = [l.strip() for l in lines[1:]]
+i = 0
+while regs[ip_reg] < len(lines):
+    inst, a, b, c = lines[regs[ip_reg]].split()
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    regs = locals()[inst](regs, a, b, c)
+    regs[ip_reg] += 1
+    i += 1
+    if i > 100:
+        print(regs[1])
+        exit()
